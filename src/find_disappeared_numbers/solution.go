c11c491369c884507e698250f548142d92090104
Nguyễn Duy Khánh <khanh.nd4@kiotviet.com>
2023-09-05 09:39:24 +0700

    find disappeared numbers

--- src/find_disappeared_numbers/solution.go
+++ src/find_disappeared_numbers/solution.go
@@ -1,5 +1,22 @@
 package find_disappeared_numbers
 
+/*
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+	Input: nums = [4,3,2,7,8,2,3,1]
+	Output: [5,6]
+
+Example 2:
+	Input: nums = [1,1]
+	Output: [2]
+
+Constraints:
+ - n == nums.length
+ - 1 <= n <= 105
+ - 1 <= nums[i] <= n
+*/
+
 func findDisappearedNumbers(nums []int) []int {
 	i := 0
 	for i < len(nums) {
