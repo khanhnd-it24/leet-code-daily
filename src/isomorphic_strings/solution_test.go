d32aca9540e9ad8a81fa8bd899fef2c9543ad2ca
Nguyễn Duy Khánh <khanh.nd4@kiotviet.com>
2023-09-05 09:46:05 +0700

    isomorphic strings

--- src/isomorphic_strings/solution_test.go
+++ src/isomorphic_strings/solution_test.go
@@ -6,6 +6,30 @@ import (
 	"testing"
 )
 
+/*
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two
+characters may map to the same character, but a character may map to itself.
+
+Example 1:
+	Input: s = "egg", t = "add"
+	Output: true
+
+Example 2:
+	Input: s = "foo", t = "bar"
+	Output: false
+
+Example 3:
+	Input: s = "paper", t = "title"
+	Output: true
+
+Constraints:
+ - 1 <= s.length <= 5 * 104
+ - t.length == s.length
+ - s and t consist of any valid ascii character.
+*/
+
 func isIsomorphic(s string, t string) bool {
 	_len := len(s)
 	sMap := make(map[byte]byte)
@@ -35,18 +59,18 @@ type Isomorphic struct {
 }
 
 var testcase = []domains.Testcase{
-	//{
-	//	In:  Isomorphic{S: "egg", T: "add"},
-	//	Out: true,
-	//},
-	//{
-	//	In:  Isomorphic{S: "foo", T: "bar"},
-	//	Out: false,
-	//},
-	//{
-	//	In:  Isomorphic{S: "paper", T: "title"},
-	//	Out: true,
-	//},
+	{
+		In:  Isomorphic{S: "egg", T: "add"},
+		Out: true,
+	},
+	{
+		In:  Isomorphic{S: "foo", T: "bar"},
+		Out: false,
+	},
+	{
+		In:  Isomorphic{S: "paper", T: "title"},
+		Out: true,
+	},
 	{
 		In:  Isomorphic{S: "bbbaaaba", T: "aaabbbba"},
 		Out: false,
