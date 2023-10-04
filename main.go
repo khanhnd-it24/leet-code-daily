bfd82a2e1f91ea0e8468922f2b8d13bcb565d074
Nguyễn Duy Khánh <khanh.nd4@kiotviet.com>
2023-10-04 10:03:31 +0700

    design hashmap linked in

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import "leet-code/src/design_hashmap"
+
 func main() {
-	//bits.Reverse()
-	//unicode.IsLetter()
+	hashMap := design_hashmap.Constructor2()
+
+	hashMap.Put(1, 1)
+	hashMap.Put(3, 4)
+	hashMap.Put(2, 2)
+
+	hashMap.Get(2)
+	hashMap.Get(3)
+
+	hashMap.Remove(2)
+	hashMap.Get(2)
 }
--- src/design_hashmap/solution.go
+++ src/design_hashmap/solution.go
@@ -1,5 +1,6 @@
 package design_hashmap
 
+// using map
 type MyHashMap struct {
 	mp map[int]int
 }
@@ -22,4 +23,76 @@ func (this *MyHashMap) Get(key int) int {
 
 func (this *MyHashMap) Remove(key int) {
 	delete(this.mp, key)
+}
+
+// using linked list
+const size = 10001
+
+type MyHashMap2 struct {
+	list [size]*Node
+}
+
+type Node struct {
+	key, val int
+	next     *Node
+}
+
+func Constructor2() MyHashMap2 {
+	return MyHashMap2{}
+}
+
+func (this *MyHashMap2) Put(key int, value int) {
+	hashed := key % size
+	if this.list[hashed] == nil {
+		this.list[hashed] = &Node{key, value, nil}
+		return
+	}
+
+	node := this.list[hashed]
+	if node.key == key {
+		node.val = value
+		return
+	}
+
+	for node.next != nil {
+		if node.next.key == key {
+			node.next.val = value
+			return
+		}
+		node = node.next
+	}
+
+	node.next = &Node{key, value, nil}
+}
+
+func (this *MyHashMap2) Get(key int) int {
+	hashed := key % size
+	for node := this.list[hashed]; node != nil; node = node.next {
+		if node.key == key {
+			return node.val
+		}
+	}
+
+	return -1
+}
+
+func (this *MyHashMap2) Remove(key int) {
+	hashed := key % size
+	node := this.list[hashed]
+	if node == nil {
+		return
+	}
+
+	if node.key == key {
+		this.list[hashed] = node.next
+		return
+	}
+
+	for node.next != nil {
+		if node.next.key == key {
+			node.next = node.next.next
+			return
+		}
+		node = node.next
+	}
 }
