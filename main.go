18639fb7e539da6b0386af5f1d977310666b560c
Nguyễn Duy Khánh <khanh.nd4@kiotviet.com>
2023-10-04 10:03:43 +0700

    design hashmap linked in

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import "leet-code/src/design_hashmap"
-
 func main() {
-	hashMap := design_hashmap.Constructor2()
 
-	hashMap.Put(1, 1)
-	hashMap.Put(3, 4)
-	hashMap.Put(2, 2)
-
-	hashMap.Get(2)
-	hashMap.Get(3)
-
-	hashMap.Remove(2)
-	hashMap.Get(2)
 }
