346d99ee8526689d3e403af1a0bb6594b20c855b
Nguyễn Duy Khánh <khanh.nd4@kiotviet.com>
2024-03-18 11:25:52 +0700

    increasing triplet subsequence

--- src/increasing_triplet_subsequence/solution_test.go
+++ src/increasing_triplet_subsequence/solution_test.go
@@ -1,1 +1,34 @@
 package increasing_triplet_subsequence
+
+import (
+	"github.com/stretchr/testify/assert"
+	"leet-code/src/core/domains"
+	"testing"
+)
+
+var testcase = []domains.Testcase{
+	{
+		In:  []int{0, 4, 2, 1, 0, -1, -3},
+		Out: false,
+	},
+	{
+		In:  []int{1, 2, 3, 4, 5},
+		Out: true,
+	},
+	{
+		In:  []int{5, 4, 3, 2, 1},
+		Out: false,
+	},
+	{
+		In:  []int{2, 1, 5, 0, 4, 6},
+		Out: true,
+	},
+}
+
+func TestIncreasingTripletSubsequence(t *testing.T) {
+	for _, tt := range testcase {
+		input := tt.In.([]int)
+		output := tt.Out.(bool)
+		assert.Equal(t, output, increasingTriplet(input))
+	}
+}
