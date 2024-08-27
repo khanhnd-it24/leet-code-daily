ed5c51131e1f6110bcbf20b40c71992a03b6f85e
Khánh Nguyễn <khanhnd.it24@gmail.com>
2024-08-27 09:39:17 +0700

    feat: rank team

--- src/rank_team/solution.go
+++ src/rank_team/solution.go
@@ -53,3 +53,25 @@ func rankTeams(votes []string) string {
 	}
 	return res.String()
 }
+
+func rankTeams2(votes []string) string {
+	var count [26][26]int
+	for _, v := range votes {
+		for i, ch := range v {
+			count[ch-'A'][i]++
+		}
+	}
+	cand := []byte(votes[0])
+	sort.Slice(cand, func(i, j int) bool {
+		a := count[cand[i]-'A']
+		b := count[cand[j]-'A']
+		for rank := 0; rank < 26; rank++ {
+			if a[rank] == b[rank] {
+				continue
+			}
+			return a[rank] > b[rank]
+		}
+		return cand[i] < cand[j]
+	})
+	return string(cand)
+}
